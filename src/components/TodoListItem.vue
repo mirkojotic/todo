9c94c5456b32609b0ed41e3c4ffc799201c42b7c
mirkojotic <joticmirko@gmail.com>
2018-06-22 14:19:14 -0400

    minor modifications

--- src/components/TodoListItem.vue
+++ src/components/TodoListItem.vue
@@ -2,7 +2,7 @@
     <div class="flex p-3">
         <div @click="edit = true" class="w-3/4 text-left self-center">
             <input
-                v-if="edit"
+                v-if="edit && todo.status === 'todo'"
                 :value="todo.text"
                 @keyup.esc="edit = false"
                 @keyup.enter="finishEditing"
@@ -10,19 +10,26 @@
                 class="shadow appearance-none border rounded w-full py-1 text-grey-darker leading-tight" />
             <span v-else>{{ todo.text }}</span>
         </div>
-        <div class="w-1/4">
+        <div class="w-1/4 pl-2">
+            <button
+                @click="removeTodo(todo)"
+                class="bg-red hover:bg-red-dark text-white py-1 px-3 rounded w-1/2">
+                <icon name="trash" />
+            </button>
             <button
                 v-if="todo.status === 'todo'"
                 @click="toggleStatus(todo)"
-                class="bg-green hover:bg-green-dark text-white py-1 px-3 rounded mr-1 ml-2">
-                DONE
+                class="bg-green hover:bg-green-dark text-white py-1 px-3 rounded w-1/2">
+                <icon name="check-square" />
             </button>
+            <!--
             <button
                 v-else
                 @click="toggleStatus(todo)"
                 class="bg-red hover:bg-red-dark text-white py-1 px-3 rounded mr-1 ml-2">
                 TODO
             </button>
+            -->
         </div>
     </div>
 </template>
@@ -36,7 +43,7 @@ export default {
     }
   },
   methods: {
-    ...mapActions(['toggleStatus', 'editTodo']),
+    ...mapActions(['toggleStatus', 'editTodo', 'removeTodo']),
     finishEditing (event) {
       this.editTodo({ ...this.todo, text: event.target.value })
       this.edit = false
--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'vue-awesome/icons/trash'
+import 'vue-awesome/icons/check-square'
 
+import Icon from 'vue-awesome/components/Icon'
+Vue.component('icon', Icon)
 Vue.config.productionTip = false
 
 new Vue({
